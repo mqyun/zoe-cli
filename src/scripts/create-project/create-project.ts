39cb91fff91d8399df8e26622d3946cc0bc10a17
mqyun <17855831290@163.com>
2021-04-15 19:32:21 +0800

    feat: createProject fun

--- src/scripts/create-project/create-project.ts
+++ src/scripts/create-project/create-project.ts
@@ -3,22 +3,24 @@ import { exec } from 'child_process';
 
 import { promptList, getCmdStr } from './prompt-config';
 
-export default async function () {
-  const { projectName, language } = await inquirer.prompt(promptList);
-  console.log('项目创建中……');
+export default function () {
+  inquirer.prompt(promptList).then(({ projectName, language }) => {
+    console.log('项目创建中……');
 
-  const cwd = process.cwd();
-  exec(getCmdStr({
-    projectName,
-    language
-  }), { cwd }, function(err) {
-    if (err) {
-      // 截取错误提示并在控制台输出
-      const failStrIndex = err.message.indexOf('fatal:');
-      console.log('项目创建失败');
-      console.log('失败原因：', err.message.slice(failStrIndex));
-      return;
-    }
-    console.log('项目创建成功');
+    const cwd = process.cwd();
+    const cmdStr = getCmdStr({
+      projectName,
+      language
+    });
+    exec(cmdStr, { cwd }, function(err) {
+      if (err) {
+        // 截取错误提示并在控制台输出
+        const failStrIndex = err.message.indexOf('fatal:');
+        console.log('项目创建失败');
+        console.log('失败原因：', err.message.slice(failStrIndex));
+        return;
+      }
+      console.log('项目创建成功');
+    });
   });
 }
